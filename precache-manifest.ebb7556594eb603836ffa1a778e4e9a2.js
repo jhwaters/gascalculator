2ba53639fc527255f7e79b00038050bb766bf802
JW <>
2020-03-24 21:42:56 -0400

    Updates

---
+++ precache-manifest.ebb7556594eb603836ffa1a778e4e9a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1dcb619da86c61516ca7479ff4ab8a",
+    "url": "/gascalculator/index.html"
+  },
+  {
+    "revision": "f0ce349d13859b1bb618",
+    "url": "/gascalculator/static/css/main.4051573a.chunk.css"
+  },
+  {
+    "revision": "cd25fadc8105a6a46300",
+    "url": "/gascalculator/static/js/2.385cb4fe.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/gascalculator/static/js/2.385cb4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ce349d13859b1bb618",
+    "url": "/gascalculator/static/js/main.42c59cdb.chunk.js"
+  },
+  {
+    "revision": "552e08c83e45834853d0",
+    "url": "/gascalculator/static/js/runtime-main.a911beb6.js"
+  }
+]);
